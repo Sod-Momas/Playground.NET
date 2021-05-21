2e2f0e250211be5f378c2c17fa8b8aa0ca84ae50
Sod-Momas <sothereer@gmail.com>
2021-05-22 01:09:12 +0800

    update Default.aspx

--- MessageBoard/Default.aspx
+++ MessageBoard/Default.aspx
@@ -2,14 +2,15 @@
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 
-    <div class="jumbotron">
+    <%--    <div class="jumbotron">
         <h1>ASP.NET</h1>
         <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
         <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
-    </div>
+    </div>--%>
 
     <div class="row">
-        <div class="col-md-4">
+
+        <%--        <div class="col-md-4">
             <h2>Getting started</h2>
             <p>
                 ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
@@ -36,6 +37,48 @@
             <p>
                 <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301950">Learn more &raquo;</a>
             </p>
+        </div>
+        --%>
+
+        <div class="row col-lg-6 col-lg-offset-3">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th colspan="2">
+                            <label class="page-header">用户登录</label>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <label for="TextName">用户名</label></td>
+                        <td>
+                            <asp:TextBox ID="TextName" runat="server" CssClass=" form-control"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="TextPwd">密码</label>
+                        </td>
+                        <td>
+                            <asp:TextBox ID="TextPwd" runat="server" TextMode="Password" CssClass=" form-control"></asp:TextBox>
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <asp:LinkButton ID="LinkLogin" runat="server" OnClick="LinkLogin_Click" CssClass="btn btn-default btn-success">登录</asp:LinkButton>
+                            &nbsp;
+                <asp:LinkButton ID="LinkReg" runat="server" PostBackUrl="~/Reg.aspx" CssClass="btn btn-default">注册</asp:LinkButton>
+                            &nbsp;
+                <asp:LinkButton ID="LinkRePwd" runat="server" OnClick="LinkRePwd_Click" CssClass="btn btn-danger">忘记密码</asp:LinkButton>
+                            &nbsp;
+                <asp:LinkButton ID="LinkEdit" runat="server" OnClick="LinkEdit_Click" CssClass="btn btn-info">修改信息</asp:LinkButton>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 
--- MessageBoard/Default.aspx.cs
+++ MessageBoard/Default.aspx.cs
@@ -4,14 +4,97 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Security;
 
 namespace MessageBoard
 {
     public partial class _Default : Page
     {
+        private static string ConnString = ConfigurationManager.ConnectionStrings["ConnString2017"].ConnectionString;
+        private SqlConnection Conn = new SqlConnection(ConnString);
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        protected void LinkLogin_Click(object sender, EventArgs e)
+        {
+            //设置Conn对象的连接字符串
+            string ConnSql =
+                System.Configuration.ConfigurationManager.ConnectionStrings["ConnString2017"].ConnectionString;
+            //声明Conn为一个SQL Server连接对象
+            SqlConnection Conn = new SqlConnection(ConnSql);
+            Conn.Open();
+            //使用MD5算法加密用户口令
+            string SecPwd = FormsAuthentication.HashPasswordForStoringInConfigFile(TextPwd.Text, "MD5");
+            string SelectSql = string.Format(" SELECT * FROM userinfo WHERE uname = N'{0}' AND upwd = '{1}' ", TextName.Text.Trim(), SecPwd);
+            SqlDataAdapter da = new SqlDataAdapter();  //创建一个空DataAdapter对象
+            da.SelectCommand = new SqlCommand(SelectSql, Conn);
+            DataSet ds = new DataSet();    //创建一个空dataSet
+                                           //将AdtaAdapter执行SQL语句返回的结果填充到DataSet对象 
+            da.Fill(ds);
+            Conn.Close();
+            if (ds.Tables[0].Rows.Count == 0)
+            {
+                //如果返回的记录条数为0，则没有条件的用户
+                Response.Write("<script>alert('用户名或密码错误！');</script>");
+                return;
+            }
+            //从DataSet中得到要修改的行
+            //DataRow MyRow = ds.Tables[0].Rows[0];
+            //if (MyRow[3].ToString().Trim()=="admin")
+            //{
+            //    //如果索引值为3的列的值为admin
+            //    Response.Write("<script>alert('你的级别是：管理员！');</script>");
+            //}
+            //else
+            //{
+            //    Response.Write("<script>alert('你的级别是：普通用户！');</script>");
+
+            //}
+            Session["userlevel"] = ds.Tables[0].Rows[0][3].ToString().Trim();
+            Session["username"] = TextName.Text;
+            Response.Redirect("~/Index2.aspx");
+        }
+
+        protected void LinkRePwd_Click(object sender, EventArgs e)
+        {
+            if (TextName.Text == "")
+            {
+                Response.Write("<script>alert('请输入用户名！');</script>");
+                return;
+            }
+            //通过查询字符串将用户名传递给目标页面
+            Response.Redirect(string.Format("~/Repwd.aspx?username={0}", TextName.Text));
+        }
+
+        protected void LinkEdit_Click(object sender, EventArgs e)
+        {
+
+            this.Conn.Open();
+            //使用MD5算法加密用户口令
+            string SecPwd = FormsAuthentication.HashPasswordForStoringInConfigFile(TextPwd.Text, "MD5");
+            string SelectSql = string.Format(" SELECT * FROM userinfo WHERE uname = N'{0}' AND upwd = '{1}' ", TextName.Text.Trim(), SecPwd);
+            SqlDataAdapter da = new SqlDataAdapter();  //创建一个空DataAdapter对象
+            da.SelectCommand = new SqlCommand(SelectSql, this.Conn);
+            DataSet ds = new DataSet();    //创建一个空dataSet
+                                           //将AdtaAdapter执行SQL语句返回的结果填充到DataSet对象 
+            da.Fill(ds);
+            this.Conn.Close();
+            if (ds.Tables[0].Rows.Count == 0)
+            {
+                //如果返回的记录条数为0，则没有条件的用户
+                Response.Write("<script>alert('用户名或密码错误！');</script>");
+                return;
+            }
+
+            //用户身份得到验证后，将用户级别和用户名保存到Session对象中
+            Session["userlevel"] = ds.Tables[0].Rows[0][3].ToString().Trim();
+            Session["username"] = TextName.Text;
+            Response.Redirect("Update.aspx");
         }
     }
 }
